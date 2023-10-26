0a9c296664768b5aef64d40dcf06d154836437ba
Mayowa Ayeni <mayeni@udel.edu>
2023-10-25 21:50:17 -0400

    nav and side bar created

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,8 @@
+const {Box} = require("@mui/system");
+const {styled} = require("@mui/system");
+
+const FlexBetween = styled(Box)({
+    display : "flex",
+    justifyContent : "space-between",
+    alignItems: "center"
+});
