fd8e3f09900f08064e322ae24258fa2b0f21f2bd
Mayowa Ayeni <mayeni@udel.edu>
2023-10-25 21:57:38 -0400

    small adjustment

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,8 +1,8 @@
-const {Box} = require("@mui/system");
-const {styled} = require("@mui/system");
+const { Box } = require("@mui/system");
+const { styled } = require("@mui/system");
 
 const FlexBetween = styled(Box)({
-    display : "flex",
-    justifyContent : "space-between",
-    alignItems: "center"
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
 });
