2aa0900ad4598c7e9868c715d9624f26deb1f7f9
Mayowa Ayeni <mayeni@udel.edu>
2023-10-25 20:05:25 -0400

    small changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import { Dashboard } from "@mui/icons-material";
 import { CssBaseline,ThemeProvider } from "@mui/material"; 
 import {createTheme} from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { themeSettings } from "theme";
-import Layout from "scenes/layout";
-import Dashboard from "scenes/dashboard";
+import Layout from "./scenes/layout";
+import Dashboard from "./scenes/dashboard";
 
 function App() {
   const mode = useSelector((state)=> state.global.mode);
