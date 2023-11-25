b0b196982ed3d5c40ef4d1d34060aea52154b6b2
Mayowa Ayeni <mayeni@udel.edu>
2023-11-24 19:37:14 -0500

    Mock Data and api calls

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,6 +3,8 @@ import {Box, useMediaQuery} from "@mui/material";
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Navbar from "components/Navbar";
+import { useGetUserQuery } from 'state/api';
+
 
 const Layout = () => {
   return <Box width="100%" height="100%">
